e542325dd19e5a3ef26513d03e4bd9b5aac2ed28
Jonas Mayer <jonas.mayer@tngtech.com>
2019-07-25 11:08:09 +0200

    image training hotfix 6

--- src/components/optimizers.py
+++ src/components/optimizers.py
@@ -34,6 +34,6 @@ class Optimizers:
 
 
         self.optimizer_G_ab = tf.train.AdamOptimizer(learning_rate=placeholders.lr, beta1=0.5) \
-            .minimize(losses.loss_D_a, var_list=networks.discriminator_spatial_a.var_list, global_step=placeholders.global_step)
+            .minimize(losses.loss_G_ab_final, var_list=networks.generator_ab.var_list)
         self.optimizer_G_ba = tf.train.AdamOptimizer(learning_rate=placeholders.lr, beta1=0.5) \
             .minimize(losses.loss_G_ba_final, var_list=networks.generator_ba.var_list)
